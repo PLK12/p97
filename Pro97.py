d46ff44bbbb655a1d9a68ec2b8c2e53d5f64201d
PLK12 <88220947+PLK12@users.noreply.github.com>
2023-10-29 17:35:53 -0700

    Add files via upload

---
+++ Pro97.py
@@ -0,0 +1,14 @@
+userNumber = input("enter number here:")
+import random number = random.randint(0,9)
+
+chances = 0
+while chances <5:
+    chances = chances+1,
+    
+if(userNumber = random number):
+    print("Congratulations!")
+if(userNumber != random number):
+    print("Try again")
+
+if not chances <5:
+    print("You Lose! The number is", random number)
